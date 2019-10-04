7dc9e7dcd5de03caf770c977a0cd4658bd89b816
ayauri29 <albayauri.29@gmail.com>
2019-10-03 19:15:07 -0500

    Add connection mongoDB

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
+const mongoClient = require("mongodb").MongoClient;
 
 const { port, dbUrl, secret } = config;
 const app = express();
 
 // TODO: Conección a la BD en mogodb
+var server = "mongodb://localhost:27017/localhost";
+
+    mongoClient.connect(server, function(error, db) {
+    if(error)
+    console.log("Error while connecting to database: ", error);
+    else
+    console.log("Connection established successfully");
+
+    //perform operations here
+
+    db.close();
+ })
 
 app.set('config', config);
 app.set('pkg', pkg);
