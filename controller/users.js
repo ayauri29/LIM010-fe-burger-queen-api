ff18aac1eb7ea99c16fe8c76163594c25e042e8a
marcelimxo <marcelimrondon@gmail.com>
2019-10-15 17:09:00 -0500

    getUsers controller

--- controller/users.js
+++ controller/users.js
@@ -4,11 +4,25 @@ const model = require('../models/user');
 
 module.exports = {
   getUsers: (req, res, next) => {
-    next();
-    // console.log(req, res, next);
+    model.users().find().toArray((error, users) => {
+      if (error) {
+        next(404);
+        throw error;
+      } else {
+        res.send(users);
+      }
+    });
   },
   createUsers: (req, res, next) => {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      next(400);
+    } else if (email.indexOf('@') === -1) {
+      next(400);
+    } else if (password.length < 5) {
+      next(400);
+    }
 
     const user = {
       email,
@@ -20,7 +34,7 @@ module.exports = {
       if (!doc) {
         model.users().insertOne(user);
         res.send({ sucess: true });
-        console.log('fiiiiiiiiggggggn');
+        console.log('Nuevo usuario insertado');
       } else {
         next(403);
       }
