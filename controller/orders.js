690f499a547fdac1631c1ce8c8881a3207db3b62
Marcelim Rondón <marcelimrondon@gmail.com>
2019-10-28 18:37:17 -0500

    Insert products function.  PUT method still wip

--- controller/orders.js
+++ controller/orders.js
@@ -2,32 +2,44 @@ const { ObjectID } = require('mongodb');
 const model = require('../models/orders');
 const pModel = require('../models/products');
 
+const insertProducts = (res, next, query) => {
+  model.orders().findOne(query).then((order) => {
+    if (!order) {
+      next(404);
+    } else {
+      
+      pModel.products().find({ _id: { $in: order.products.map((p) => p.productId) }}).toArray().then((products) => {
+        order.products = order.products.map((orderP) => ({
+          qty: orderP.qty,
+          product: products.find((p) => p._id.equals(orderP.productId)),
+        }))
+        // console.log('order', order);
+        res.send(order);
+      })        
+    }
+  });
+};
+
 module.exports = {
   createOrder: (req, res, next) => {
     const { userId, client, products } = req.body;
     if (!userId || !products) {
       next(400);
     } else {
-      products.forEach(elem => {
-        pModel.products().findOne({ _id: new ObjectID(elem.productId) })
-          .then((product) => {
-            const order = {
-              userId,
-              client,
-              // products: [{ product: { productId: product._id, name: product.name, price: product.price }, qty: elem.qty }],
-              products: [{ product: { productId: product._id, name: product.name, price: product.price }, qty: elem.qty }],
-              status: 'pending',
-              dateEntry: new Date(),
-              dateProcessed: ''
-            };
-            model.orders().insertOne(order, (err, result) => {
-              if (!err) {
-                console.log('result', result.ops);
-                res.send(result.ops[0]);
-              }
-            });
-
-          })
+      const order = {
+        userId,
+        client,
+        products: products.map((p) => ({ productId: new ObjectID(p.productId), qty: p.qty })),
+        status: 'pending',
+        dateEntry: new Date(),
+        dateProcessed: ''
+      };
+      
+      model.orders().insertOne(order, (err, result) => {
+        if (!err) {
+          // console.log('result', result.ops);
+          insertProducts(res, next, { _id: new ObjectID(result.ops[0]._id) });
+        }
       });
     }
   },
@@ -55,18 +67,55 @@ module.exports = {
   getOrderById: (req, res, next) => {
     const hex = /[0-9A-Fa-f]{6}/g;
     const reqParam = req.params.orderid;
-    console.log('req', req.params);
     const query = (hex.test(reqParam)) ? { _id: new ObjectID(reqParam) } : { _id: reqParam };
+    insertProducts(res, next, query);
+  },
+  deleteOrderById: (req, res, next) => {
+    const hex = /[0-9A-Fa-f]{6}/g;
+    const reqParam = req.params.orderid;
+    const query = (hex.test(reqParam)) ? { _id: new ObjectID(reqParam) } : { _id: reqParam };
+    
     model.orders().findOne(query).then((order) => {
       if (!order) {
         next(404);
       } else {
-        console.log('order', order);
-        return res.send(order);
+        // console.log('Casi');
+        model.orders().deleteOne({ _id: order._id }, (err) => {
+          // console.log('Entre');
+          if (!err) {
+            res.send(order);
+          }
+        });
       }
     });
   },
-/*   deleteOrderById: (req, res,next) => {
+  putOrderById: (req, res, next) => {
+    const hex = /[0-9A-Fa-f]{6}/g;
+    const reqParam = req.params.orderid;
+    const query = (hex.test(reqParam)) ? { _id: new ObjectID(reqParam) } : { _id: reqParam };
+    console.log('body', req.body);
+    const { userId, client, products, status } = req.body;
+    
+    model.orders().findOne(query).then((order) => {
+      console.log('order', order);
+      if (!order) {
+        next(404);
+      } else {
 
-  }, */
+        model.orders().findAndModify(query, [], {
+          $set: {
+            products: products || order.products,
+            status: status || order.status,
+            dateProcessed: status === 'delivered' ? new Date() : order.dateProcessed,
+          },
+        }, { new: true }, (err, result) => {
+          console.log('result put', result.value);
+          if (err) {
+            console.log('no se modifico');
+          }
+          return res.send(result.value);
+        });
+      }
+    });
+  }
 };
